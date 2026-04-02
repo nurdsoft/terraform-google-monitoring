efe8dae239d84b6c3280cff77d5ca574419ffcc7
chinmay jain <jchinmay654@gmail.com>
2026-04-02 23:20:36 +0530

    updated format of examples main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,28 +16,28 @@ module "monitoring" {
   notification_channel_display_name = var.notification_channel_display_name
 
   # Cloud Function configuration
-  region               = var.region
-  function_name        = var.function_name
-  function_description = var.function_description
-  function_source_dir  = "${path.module}/sample-function"
-  function_runtime     = var.function_runtime
-  function_entry_point = var.function_entry_point
-  function_bucket_name = var.function_bucket_name
-  max_instance_count   = var.max_instance_count
-  available_memory     = var.available_memory
-  timeout_seconds      = var.timeout_seconds
+  region                = var.region
+  function_name         = var.function_name
+  function_description  = var.function_description
+  function_source_dir   = "${path.module}/sample-function"
+  function_runtime      = var.function_runtime
+  function_entry_point  = var.function_entry_point
+  function_bucket_name  = var.function_bucket_name
+  max_instance_count    = var.max_instance_count
+  available_memory      = var.available_memory
+  timeout_seconds       = var.timeout_seconds
   environment_variables = var.environment_variables
 
   # Log Metric configuration
-  metric_name            = var.metric_name
-  metric_bucket_name     = var.metric_bucket_name
-  metric_filter          = var.metric_filter
-  metric_kind            = var.metric_kind
-  metric_value_type      = var.metric_value_type
-  metric_labels          = var.metric_labels
+  metric_name             = var.metric_name
+  metric_bucket_name      = var.metric_bucket_name
+  metric_filter           = var.metric_filter
+  metric_kind             = var.metric_kind
+  metric_value_type       = var.metric_value_type
+  metric_labels           = var.metric_labels
   metric_label_extractors = var.metric_label_extractors
-  wait_for_metric        = var.wait_for_metric
-  wait_duration          = var.wait_duration
+  wait_for_metric         = var.wait_for_metric
+  wait_duration           = var.wait_duration
 
   # Alert Policies configuration
   notification_channels           = [module.monitoring.notification_channel_name]
